81b20c4772d1d0d4702fabadcfe6199f09b29692
DenisTis <tislenko.denis@gmail.com>
2017-10-29 10:58:58 +0100

    first working translation

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import I18n from 'i18n-js';
+I18n.fallbacks = true;
+I18n.defaultLocale = 'en'
+I18n.locale = 'de';
+I18n.translations = {
+  en: {
+    enter: 'Enter!'
+  },
+  ru: {
+    enter: 'Vhod!'
+  },
+  de: {
+    enter: 'Eingang!'
+  }
+}
+//export default I18n - to provide it to all other classes
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Icon name='area-chart' size={30} color="#900" />
-      <Text>Lisa privet </Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Icon name='paw' size={80} color="orange" style={styles.icon} />
+        <Text style={styles.text}>
+        {I18n.t('enter')}
+        </Text>
       </View>
     );
   }
@@ -19,9 +37,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',  
-    alignItems: 'center',  
+    backgroundColor: '#c5eafd',
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'lightblue',
-  }, 
+  },
+  icon: {
+    paddingBottom: 20,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
 });
