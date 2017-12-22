5005b0ea8975314e80b7c606d0f17e23d0bd7576
DenisTis <tislenko.denis@gmail.com>
2017-12-22 20:48:15 +0100

    blender object is loaded

--- web/components/MapNavigation.jsx
+++ web/components/MapNavigation.jsx
@@ -33,36 +33,66 @@ export default class MapNavigationPage extends React.Component {
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.minDistance = 3;
-    controls.maxDistance = 10;
+    //    controls.maxDistance = 10;
     // controls.minAzimuthAngle = Math.PI / 2; //do not let go below ground
     // controls.maxAzimuthAngle = 0; //do not let go below ground
     controls.minPolarAngle = 0; //inclination to look top-down
     controls.maxPolarAngle = Math.PI / 2.5; // 90% inclination to ground
 
     //Start adding geometries
-    const planeGeometry = new THREE.PlaneGeometry(10, 10);
-    const planeMaterial = new THREE.MeshLambertMaterial({
-      color: 0xe69900,
-      side: THREE.DoubleSide
-    });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // const planeGeometry = new THREE.PlaneGeometry(10, 10);
+    // const planeMaterial = new THREE.MeshLambertMaterial({
+    //   color: 0xe69900,
+    //   side: THREE.DoubleSide
+    // });
+    // const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // plane.receiveShadow = true;
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshLambertMaterial({ color: "#ff0000" });
     const cube = new THREE.Mesh(geometry, material);
     cube.position.z = 1;
+    cube.castShadow = true;
 
     //Stop adding geometries
-    scene.add(plane);
+    //scene.add(plane);
     scene.add(cube);
 
     //Add lights
     var ambientLight = new THREE.AmbientLight(0x0c0c0c);
     scene.add(ambientLight);
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-30, 60, 60);
-    spotLight.castShadow = true;
-    scene.add(spotLight);
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    var mainLight = new THREE.DirectionalLight();
+    mainLight.position.set(0, -17, 30);
+    mainLight.castShadow = true;
+    scene.add(mainLight);
+
+    var jLoader = new THREE.JSONLoader();
+    jLoader.load("assets/map.json", function(geometry, materials) {
+      let object = new THREE.Mesh(
+        geometry,
+        new THREE.MeshLambertMaterial({ color: "#23467f" })
+      );
+      //        model = object;
+      //object.scale = 0.5;
+      object.scale.set(0.5, 0.5, 0.5);
+      object.position.y = 1;
+      object.rotation.x = Math.PI / 2;
+      object.receiveShadow = true;
+      object.position.z = -1;
+      scene.add(object);
+      //        addPhysicModel(geometry);
+    });
+    // var helper = new THREE.CameraHelper(mainLight.shadow.camera);
+    // scene.add(helper);
+
+    // var spotLight = new THREE.SpotLight(0xffffff);
+    // spotLight.position.set(-2, 0, 0);
+    // spotLight.castShadow = true;
+    // scene.add(spotLight);
 
     this.scene = scene;
     this.camera = camera;
